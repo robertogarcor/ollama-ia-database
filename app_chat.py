5ea9362f642aded44d45b3f605b27b1070c41175
robertogarcor <groberto74@gmail.com>
2025-04-10 13:49:32 +0200

    App chat llm ollama

---
+++ app_chat.py
@@ -0,0 +1,62 @@
+import logging
+import os
+import requests
+from pydantic import BaseModel, ValidationError
+from dotenv import load_dotenv
+
+load_dotenv()
+logger = logging.getLogger(__name__)
+
+ENDPOINT_LLM = os.getenv('URL_SERVER_OLLAMA_LOCAL')
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+def main() -> None:
+    while True:
+        question = input('User: ')
+        if question.lower() == 'quit' or question.lower() == 'q':
+            print('Adios!')
+            break
+        response = chat(question)
+        print('IA: ', response)
+        print()
+
+
+def chat(question) -> str | None:
+    url = ENDPOINT_LLM
+    model = os.getenv('MODEL_LLM_OLLAMA_LOCAL')
+    messages = [
+        {
+            'role': 'system',
+            'content': 'Te llamas Llama y eres una persona ironica. Experto en BBDD',
+        },
+        {
+            'role': 'user',
+            'content': question,
+        },
+    ]
+    try:
+        response = requests.post(
+            url,
+            json={
+                'model': model,
+                'messages': messages,
+                'stream': False,
+            },
+        )
+        response.raise_for_status()
+        response_json = response.json()
+        message = Message(**response_json['message'])
+        return message.content
+    except requests.RequestException as e:
+        print(f'Error occurred: {e}')
+        return None
+    except ValidationError as ve:
+        print(f'Error parser response {ve}')
+        return None
+
+
+if __name__ == '__main__':
+    main()
