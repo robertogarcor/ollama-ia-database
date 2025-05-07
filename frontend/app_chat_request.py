8c8136ff6afcd03a206f092444de9844a545b910
robertogarcor <groberto74@gmail.com>
2025-05-07 09:18:38 +0200

    update load and save history file json

--- frontend/app_chat_request.py
+++ frontend/app_chat_request.py
@@ -15,14 +15,17 @@ HISTORY_FILE = "chat_history_messages.json"
 # Function load messages history
 def load_history() -> json:
     if os.path.exists(HISTORY_FILE):
-        with open(HISTORY_FILE, "r", encoding="utf-8") as f:
-            return json.load(f)
+        try:
+            with open(HISTORY_FILE, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            return []
     return []
 
 # Function save messages history
 def save_history(history) -> None:
     with open(HISTORY_FILE, "w") as f:
-        json.dump(history, f, ensure_ascii=False, indent=2)
+        json.dump(history or [], f, ensure_ascii=False, indent=2)
 
 # Function show message html
 def show_message_html(author: str, date_message: datetime, message: str, chat_html) -> str:
@@ -52,7 +55,7 @@ if "history" not in st.session_state:
 st.set_page_config(page_title="Chat IA - Soporte Tickets", page_icon=None)
 st.title("💬 Asistente IA - Soporte Tickets")
 # Question User Input
-question_user = st.text_area('Escribe tu consulta:', key='user_input', placeholder='Aqui tu consulta', height=80)
+question_user = st.text_input('Escribe tu consulta:', key='user_input', placeholder='Aqui tu consulta')
 current_date = datetime.datetime.now()
 # If Click Btn Enviar and question user true
 if st.button("Enviar") and question_user:
@@ -66,10 +69,18 @@ if st.button("Enviar") and question_user:
             )
             # Response assistant IA Server
             date_response = response.json()['date_response']
-            response = response.json()['response']
+            data_response = response.json()['response']
         # Add to History
-        st.session_state.chat_history.append(("Tú", date_question_user, question_user))
-        st.session_state.chat_history.append(("Bot", date_response, response))
+        st.session_state.chat_history.append({
+            "role": "Tú",
+            "timestamp": date_question_user,
+            "message": question_user
+        })
+        st.session_state.chat_history.append({
+            "role": "Bot",
+            "timestamp": date_response,
+            "message": data_response
+        })
         st.session_state.current_response = ""
         # Save History from JSON
         save_history(st.session_state.chat_history)
@@ -118,7 +129,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 chat_html = '<div class="chat-container">'
-for author, date_message, message in reversed(st.session_state.chat_history):
+for entry in reversed(st.session_state.chat_history):
+    author = entry["role"]
+    date_message = entry["timestamp"]
+    message = entry["message"]
     chat_html = show_message_html(author, date_message, message, chat_html)
 chat_html += '</div>'
 st.markdown(chat_html, unsafe_allow_html=True)
